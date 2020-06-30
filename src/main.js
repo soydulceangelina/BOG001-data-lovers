61e7398621e92496bff6e1e67170d1b89637a706
Dulce Mercado <soydulceangelina@gmail.com>
2020-06-29 19:36:14 -0500

    new page

--- src/main.js
+++ src/main.js
@@ -10,19 +10,19 @@ const printCharacters = () => {
             candyCount = 0
         }
         return`
-        <article class="pokemon-item">
-            <a href="#/${character.id}/" class="card-container">
-                <img src="${character.img}" alt="${character.name}"
-                <h2>${character.name}</h2>
-            </a>
-            <h1 class="details-items">Detalles</h1>
-            <section class="details-container">
-                <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
-                <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
-                <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
-                <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
-            </section>
-        </article>`
+            <article class="pokemon-item">
+                <a href="#/${character.id}/" class="card-container">
+                    <img src="${character.img}" alt="${character.name}"
+                    <h2>${character.name}</h2>
+                </a>
+                <h1 class="details-items">Detalles</h1>
+                <section class="details-container">
+                    <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
+                    <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
+                    <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
+                    <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
+                </section>
+            </article>`
     }).join('');
 
     root.innerHTML = view;
