7b9b7cb35d5421110a77056310e5f3c6daeb1227
Sara <saritacorredor15@gmail.com>
2020-07-03 15:52:20 +0200

    cambios

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
-estas funciones son de ejemplo
+//funciones para ordenar de forma descendete y ascendente
+// Se factorizan todas las funciones para que devuelven el array en el orden deseado
+function sortNumDesc(arrayPokemones) {
+  let arrPokemonesNumDesc = arrayPokemones;
+  arrPokemonesNumDesc = arrPokemonesNumDesc.sort((a, b) => {
+    if (parseInt(a.num) < parseInt(b.num))
+      return 1
+    else
+      return -1
+  })
+  return arrPokemonesNumDesc
+}
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-return 'OMG';
-};
+function sortNumAsc(arrayPokemones) {
+  let arrPokemonesNumAsc = arrayPokemones;
+  arrPokemonesNumAsc = arrPokemonesNumAsc.sort((a, b) => {
+    if (parseInt(a.num) > parseInt(b.num))
+      return 1
+    else
+      return -1
+  })
+  return arrPokemonesNumAsc
+}
--- src/main.js
+++ src/main.js
@@ -1,160 +1,81 @@
 // import { example } from './data.js';
-
 import data from './data/pokemon/pokemon.js';
-
 const dataPokemon = data.pokemon;
-
 const root = document.getElementById("root");
 
-
-
 const printCharacters = () => {
-
     const view = dataPokemon.map((character) => {
-
         let candyCount = character.candy_count;
-
         if(candyCount === undefined){
-
             candyCount = 0
-
         }
-
         return`
-
-            <article class="pokemon-item">
-
-                <a href="#/${character.id}/" class="card-container">
-
-                    <img src="${character.img}" alt="${character.name}"
-
-                    <h2>${character.name}</h2>
-
-                </a>
-
-                <h1 class="details-items">Detalles</h1>
-
-                <section class="details-container">
-
-                    <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
-
-                    <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
-
-                    <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
-
-                    <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
-
-                </section>
-
-            </article>`
-
+        <article class="pokemon-item">
+            <a href="#/${character.id}/" class="card-container">
+                <img src="${character.img}" alt="${character.name}"
+                <h2>${character.name}</h2>
+            </a>
+            <h1 class="details-items">Detalles</h1>
+            <section class="details-container">
+                <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
+                <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
+                <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
+                <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
+            </section>
+        </article>`
     }).join('');
 
-
-
     root.innerHTML = view;
-
  };
 
-
-
  function addEventsToCards() {
-
     let pokeCards = document.querySelectorAll(".pokemon-item");
-
     pokeCards.forEach(card => {
-
         let pokeDetails = card.querySelector(".details-container")
-
         const showDetails = () =>{
-
             pokeDetails.classList.add("is-open");
-
         }
-
     
-
         const hideDetails = () =>{
-
             pokeDetails.classList.remove("is-open");
-
         }
-
         
-
         card.addEventListener("mouseover", showDetails);
-
         card.addEventListener("mouseout", hideDetails);
-
     })
-
  }
 
-
-
 window.addEventListener("load", () => {
-
     printCharacters()
-
     addEventsToCards()
-
 });
-
-
 
 // -----------------------------------------------------------------------------------------
 
-
-
 let menu = document.querySelector(".filter-bar");
-
 const burgerButton = document.querySelector("#burgerMenu");
 
-
-
 const hideShow = () => {
-
     if(menu.classList.contains("is-active")){
-
         menu.classList.remove("is-active");
-
     }else{
-
         menu.classList.add("is-active");
-
     }
-
 };
-
-
 
 burgerButton.addEventListener("click", hideShow);
 
-
-
 // ---------------------------------------------------------------------------------------------
 
-
-
 let infoIcons = document.querySelector(".info-bar");
-
 const infoIconsBotton = document.querySelector("#infoMenu");
 
-
-
 const hideShowInfoIcons = () => {
-
     if(infoIcons.classList.contains("is-active")){
-
         infoIcons.classList.remove("is-active");
-
     }else{
-
         infoIcons.classList.add("is-active");
-
     }
-
 };
-
 
 infoIconsBotton.addEventListener("click", hideShowInfoIcons);
