0c395bb8f05fc53f5731e421ef5621bd3206a67e
Dulce Mercado <soydulceangelina@gmail.com>
2020-07-08 09:48:50 -0500

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,48 @@
-// estas funciones son de ejemplo
+import data from './data/pokemon/pokemon.js';
 
-export const example = () => {
-  return 'example';
+export const filterByType = (typePoke, pokeData = data.pokemon) => {
+  if(typePoke === ""){
+    return pokeData;
+  }else{
+    return pokeData.filter(poke => {
+      return poke.type.includes(typePoke)
+    })
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const alfabeticFilter = (alfa, pokeData = data.pokemon) => {
+  const pokemons = [...pokeData]
+  if (alfa === ""){
+    return pokemons;
+  }
+  if (alfa === "a-z"){
+    return pokemons.sort((prev, next) => {
+      if(prev.name > next.name){
+        return 1;  
+      } else {
+        return -1;
+      }
+    })
+
+  }
+  if(alfa === "z-a"){
+    return pokemons.sort((prev, next) => {
+      if(prev.name < next.name){
+        return 1;  
+      } else {
+        return -1;
+      } 
+    })
+  }
+}
+
+export const searchPokemon = (order, pokeData = data.pokemon) => {
+  if(order === "") {
+    return pokeData;
+  }
+  else{
+    return pokeData.filter (poke => {
+      return poke.name.toLowerCase().startsWith(order.toLowerCase())
+    })
+  }
+}
--- main.js
+++ main.js
@@ -1,54 +1,109 @@
-// import { example } from './data.js';
+import { filterByType } from './data.js';
+import { alfabeticFilter } from './data.js';
+import { searchPokemon } from './data.js';
 import data from './data/pokemon/pokemon.js';
 const dataPokemon = data.pokemon;
 const root = document.getElementById("root");
+const typeFilter = document.getElementById("typeFilter");
+const ordenFilter = document.getElementById("ordenFilter");
+const search = document.getElementById("search");
 
-const printCharacters = () => {
-    const view = dataPokemon.map((character) => {
-        let candyCount = character.candy_count;
-        if(candyCount === undefined){
-            candyCount = 0
-        }
-        return`
-            <article class="pokemon-item">
-                <a href="#/${character.id}/" class="card-container">
-                    <img src="${character.img}" alt="${character.name}"
-                    <h2>${character.name}</h2>
-                </a>
-                <h1 class="details-items">Detalles</h1>
-                <section class="details-container">
-                    <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
-                    <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
-                    <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
-                    <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
-                </section>
-            </article>`
-    }).join('');
+export const printCharacters = (avatars) => {
+        const view = avatars.map((character) => {
+            let candyCount = character.candy_count;
+            if(candyCount === undefined){
+                candyCount = 0
+            }
+            return`
+                <article class="pokemon-item">
+                    <a href="#/${character.id}/" class="card-container">
+                        <img src="${character.img}" alt="${character.name}"
+                        <h2>${character.name}</h2>
+                    </a>
+                    <h1 class="details-items">Detalles</h1>
+                    <section class="details-container">
+                        <h2><span class="flaticon-fengshui icons-details"></span>${character.type}</h2>
+                        <h2><span class="flaticon-dimension-of-line-height icons-details"></span>${character.height}</h2>
+                        <h2><span class="flaticon-dumbbell icons-details"></span>${character.weight}</h2>
+                        <h2><span class="flaticon-candy icons-details"></span>${candyCount} caramelos</h2>
+                    </section>
+                </article>`
+        }).join('');
 
     root.innerHTML = view;
  };
 
- function addEventsToCards() {
+const addEventsToCards = () => {
     let pokeCards = document.querySelectorAll(".pokemon-item");
     pokeCards.forEach(card => {
         let pokeDetails = card.querySelector(".details-container")
         const showDetails = () =>{
             pokeDetails.classList.add("is-open");
         }
-    
         const hideDetails = () =>{
             pokeDetails.classList.remove("is-open");
         }
-        
         card.addEventListener("mouseover", showDetails);
         card.addEventListener("mouseout", hideDetails);
     })
+
  }
 
+const hideTypeMenu = () => {
+    const types = []
+    dataPokemon.forEach(({ type }) => {
+        type.forEach(t => {
+            if (!types.includes(t)) {
+                types.push(t)
+            }
+        })
+    })
+
+    types.forEach((pokeType) => {
+        const option = document.createElement('option')
+        option.setAttribute('value', pokeType)
+        option.innerText = pokeType
+        typeFilter.appendChild(option)
+    })
+}
+
+const nonExistent = () =>{
+    root.innerHTML = "Pokémon no encontrado";
+}
+
 window.addEventListener("load", () => {
-    printCharacters()
+    printCharacters(dataPokemon)
     addEventsToCards()
+    hideTypeMenu()
 });
+
+typeFilter.addEventListener("change", (event) =>{
+    const filteredByOrder = alfabeticFilter(ordenFilter.value);
+    const filteredByName = searchPokemon(search.value, filteredByOrder);
+    const filtered = filterByType(event.target.value, filteredByName);
+    printCharacters(filtered)
+    addEventsToCards()
+})
+
+ordenFilter.addEventListener("change", (event) =>{
+    const filteredByName = searchPokemon(search.value);
+    const filteredByType = filterByType(typeFilter.value, filteredByName);
+    const filtered = alfabeticFilter(event.target.value, filteredByType);
+    printCharacters(filtered)
+    addEventsToCards()
+})
+
+search.addEventListener("keyup", (event)=> {
+    const filteredByOrder = alfabeticFilter(ordenFilter.value);
+    const filteredByType = filterByType(typeFilter.value, filteredByOrder);
+    const filtered = searchPokemon(event.target.value, filteredByType);
+    if(filtered.length === 0){
+        nonExistent();
+    }else{
+        printCharacters(filtered);
+        addEventsToCards();
+    }
+})
 
 // -----------------------------------------------------------------------------------------
 
@@ -79,3 +134,4 @@ const hideShowInfoIcons = () => {
 };
 
 infoIconsBotton.addEventListener("click", hideShowInfoIcons);
+
